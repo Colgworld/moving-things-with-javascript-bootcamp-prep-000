bd988dc8be91f726272f842fb2fcc0805897bd00
Michael Colgan <michaelwcolgan@gmail.com>
2018-10-25 16:06:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 const app = "I don't do much."
 
-document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
-    moveDodgerLeft()
-  }
-})
+var dodger = document.getElementById('dodger');
 
-document.addEventListener('keydown', function(e) {
-  if (e.which === 39) {
-    moveDodgerRight()
-  }
-})
+    document.addEventListener('keydown', function(e) {
+      if (e.which === 37) {
+        moveDodgerLeft()
+      } else if (e.which === 39) {
+        moveDodgerRight()
+      } else {
 
-function moveDodgerLeft() {
-  var leftNumbers = dodger.style.left.replace('px', '')
-  var left = parseInt(leftNumbers, 10)
+      }
+    })
 
-  if (left > 0) {
-    dodger.style.left = `${left - 1}px`
-  }
-}
+    function moveDodgerLeft() {
+      var leftNumbers = dodger.style.left.replace('px', '')
+      var left = parseInt(leftNumbers, 10)
 
-function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(rightNumbers, 10)
+      if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+      }
+    }
 
-  if (right > 0) {
-    dodger.style.right = `${right - 1}px`
-  }
-}
+    function moveDodgerRight() {
+      var rightNumbers = dodger.style.right.replace('px', '')
+      var right = parseInt(rightNumbers, 10)
+      if (right > 0) {
+        dodger.style.right = `${right - 1}px`
+      }
+    }
