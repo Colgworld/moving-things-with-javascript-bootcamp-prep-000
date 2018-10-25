adff57c7f39df4c8650a7349f7d371042e7ed5c3
Michael Colgan <michaelwcolgan@gmail.com>
2018-10-25 16:10:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const app = "I don't do much."
 var dodger = document.getElementById('dodger');
 
-function addRight() {
-    var rightClass = document.getElementById('dodger');
-    element.classList.add('right');
-}
+    $(document).ready(function(){
+        addRight()
+        });
+    });
+
+    function addRight() {
+        var rightClass = document.getElementById('dodger');
+        element.classList.add('right');
+    }
 
     document.addEventListener('keydown', function(e) {
       if (e.which === 37) {
